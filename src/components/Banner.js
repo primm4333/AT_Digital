ba77a3272c4f37856bb1c8ec8955e80d7f53c9e4
Primal Fernando <128360155+primm4333@users.noreply.github.com>
2024-12-17 20:37:29 +0530

    size adjesment

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
     <section className="relative bg-white">
       <img src="/banner.jpg" alt="Banner" className="w-full h-auto" />
     <div className="absolute bottom-0 left-0 w-full h-full flex items-end justify-start p-10">
-      <div className="bg-gradient-to-r from-[#1CBDDD] to-[#4DCA79] p-6 md:p-12 rounded-md text-white w-3/5 ">
+    <div className="bg-gradient-to-r from-[#1CBDDD] to-[#4DCA79] p-6 md:p-12 rounded-md text-white w-3/5">
       <h2 className="text-3xl md:text-4xl font-bold mb-4">
       We Crush Your Competitors, Goals, And Sales Records - Without The B.S.
       </h2>
